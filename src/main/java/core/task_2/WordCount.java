a7975e54fc8c1420df1fe285fd02d5a3e73b3435
XxxAnduXaaA <nekit991tka2017@gmail.com>
2023-07-05 21:31:37 +0300

    Сокращено и доделано

--- src/main/java/core/task_2/WordCount.java
+++ src/main/java/core/task_2/WordCount.java
@@ -1,43 +1,39 @@
 package core.task_2;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class WordCount {
     public static void main(String[] args) {
+
+        Map<String,Integer> map = new HashMap<>();
+
         try(BufferedReader bufferedReader = new BufferedReader(new FileReader("C:\\Users\\AndromedA\\IdeaProjects\\Tasks\\src\\main\\java\\core\\task_2\\text.txt"))){
             String line;
-            int counter = 0;
-
-            Map<String,Integer> map = new HashMap<>();
 
             while((line = bufferedReader.readLine()) != null){
                 String words[] = line.split(" ");
                 for(String word : words){
-                    if(map.containsKey(word)){
-                        map.put(word, map.get(word) + 1);
-                    }
-                    else{
-                        map.put(word,1);
-                    }
+                      map.put(word,map.getOrDefault(word, 0) + 1);
+
                 }
             }
 
 
-            for(Map.Entry<String,Integer> entry : map.entrySet()){
-                System.out.println("Слово " + entry.getKey() + " встречается " + entry.getValue() + " раз");
-            }
-
-
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        } catch (IOException e) {
+        }  catch (IOException e) {
             throw new RuntimeException(e);
         }
+
+        List<Map.Entry<String,Integer>> sortedList = new ArrayList<>(map.entrySet());
+        sortedList.sort(Map.Entry.comparingByValue(Comparator.reverseOrder()));
+
+        for(Map.Entry<String,Integer> entry : sortedList){
+
+            System.out.println("Слово " + entry.getKey() + " встречается " + entry.getValue() + " раз");
+        }
+
+
     }
 }
